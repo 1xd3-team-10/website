bfa159a1590c8ce611bf1ce03c912cf290ac6851
LucasJoel1 <ljoel1@outlook.com>
2026-04-01 15:57:07 -0400

    - added message sending - fixed xss vulnurability

---
+++ Team10/app/assets/js/contacts.js
@@ -0,0 +1,64 @@
+window.addEventListener(("load"), () => {
+    const contacts = document.querySelectorAll("#contact");
+    const sendMessage = document.querySelector("#sendMessage");
+    const messageInput = document.querySelector("#send_message_input");
+    const chatWindow = document.querySelector("#chatWindow");
+    let recipient = "";
+
+    contacts.forEach((contact) => {
+        contact.addEventListener("click", () => {
+            messageInput.disabled = false
+            recipient = contact.innerHTML;
+        })
+    })
+
+    sendMessage.addEventListener("click", () => {
+        send();
+    })
+
+    messageInput.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") send();
+    })
+
+    const send = () => {
+        if (messageInput.value === "") {
+            const errorMsg = document.createElement('div');
+            errorMsg.classList = "auth_error";
+            errorMsg.onclick = () => { errorMsg.style.display = 'none' }
+            errorMsg.innerText = "Please enter a valid message";
+            document.body.prepend(errorMsg);
+            return;
+        }
+        fetch("../events/sendMessage.php", {
+            method: "POST",
+            credentials: "include",
+            body: JSON.stringify({
+                recipient: recipient,
+                msg: messageInput.value
+            })
+        }).then((res) => {
+            if (res.ok) {
+                const msg = messageInput.value;
+                const chatMsg = createMessage(msg, true);
+                chatWindow.append(chatMsg);
+                scrollToBottom();
+                messageInput.value = "";
+            }
+        })
+    }
+
+    const createMessage = (msg, byUser) => {
+        const newMessage = document.createElement('div');
+        newMessage.innerText = msg;
+        if (byUser) {
+            newMessage.classList = "userMessage"
+        } else {
+            newMessage.classList = "recipientMessage"
+        }
+        return newMessage;
+    }
+
+    const scrollToBottom = () => {
+        chatWindow.scrollTop = chatWindow.scrollHeight;
+    };
+})
---
+++ Team10/app/events/sendMessage.php
@@ -0,0 +1,71 @@
+<?php
+require_once __DIR__ . "/../lib/auth/auth.php";
+require_once __DIR__ . "/../lib/connect.php";
+
+
+if (!isLoggedIn()) {
+    http_response_code(403);
+    echo json_encode(["error" => "Unauthorized"]);
+    exit;
+}
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+$msg = $data["msg"];
+$recipient = $data["recipient"];
+$senderID = getUserId();
+
+if (!$msg || $msg == "") {
+    http_response_code(400);
+    echo json_encode(["error" => "Please send a valid message"]);
+    exit;
+}
+
+if (!$recipient || $recipient == "") {
+    http_response_code(400);
+    echo json_encode(["error" => "Please select a valid recipient"]);
+    exit;
+}
+
+$conn = connect();
+
+$sql = "SELECT * FROM users WHERE username=?;";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $recipient);
+$stmt->execute();
+$res = $stmt->get_result();
+
+if ($res->num_rows !== 1) {
+    http_response_code(500);
+    echo json_encode(["success" => false, "error" => "Internal server error"]);
+    exit;
+}
+
+$recipientID = $res->fetch_assoc()["user_id"];
+
+if ($recipientID === null) {
+    http_response_code(500);
+    echo json_encode(["success" => false, "error" => "Internal server error"]);
+    exit;
+}
+
+$convID = min($senderID, $recipientID) . '-' . max($senderID, $recipientID);
+
+$sql = "INSERT INTO messages (conversation_id, sender_id, content) VALUES (?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sis", $convID, $senderID, $msg);
+
+if (!$stmt->execute()) {
+    http_response_code(500);
+    echo json_encode(["success" => false, "error" => "Database error"]);
+    exit;
+}
+
+http_response_code(201);
+echo json_encode([
+    "success" => true,
+    "conversation_id" => $convID
+]);
+
+$stmt->close();
+$conn->close();
